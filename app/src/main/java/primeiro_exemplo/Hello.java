c988e74cbe9dcd96749fa32ee86880e509956795
AlvarengaEric17 <ericolivealvarenga@gmail.com>
2024-02-16 12:25:58 +0000

    aula02sw2

--- app/src/main/java/primeiro_exemplo/Hello.java
+++ app/src/main/java/primeiro_exemplo/Hello.java
@@ -1,5 +1,7 @@
 package primeiro_exemplo;
 
+import primeiro_exemplo.modelos.Pessoa;
+
 public class Hello {
     public static void main(String[] args) {
         int cont = 10;
@@ -7,5 +9,27 @@ public class Hello {
         System.out.println("Olá Mundo!!!");
 
         System.out.println(cont + " : " + nome);
+
+        Pessoa p1 = new Pessoa();
+
+        p1.nome = "Eric";
+        p1.idade = 17;
+
+        System.out.println(p1.nome + " : " + p1.idade + " anos");
+
+        Pessoa p2 = p1;
+        p2.nome = "Isaque";
+
+        System.out.println(p2.nome);
+        System.out.println(p1.nome);
+
+
+        Pessoa p3 = new Pessoa();
+        p3.nome = "Zezinho";
+        p3.idade = 20;
+
+        System.out.println(p1.verificarMaioridade());
+        System.out.println(p3.verificarMaioridade());
     }
+
 }
---
+++ app/src/main/java/primeiro_exemplo/modelos/Pessoa.java
@@ -0,0 +1,13 @@
+package primeiro_exemplo.modelos;
+
+public class Pessoa {
+    // Atributos 
+    public String nome;
+    public int idade;
+
+    // Metodos
+    public boolean verificarMaioridade(){
+        return this.idade >= 18;
+    }
+
+}
